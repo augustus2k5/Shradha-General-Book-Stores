4c5b22923ab28dff84961be5b6a259bae25b04f8
Hoàng Hải <hoanghai03112k5@gmail.com>
2025-11-01 10:45:41 +0700

    feat(category): add create feature

---
+++ DTOs/CategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eproject2025.DTOs
+{
+    public class CategoryDTO
+    {
+        public string? CategoryId { get; set; }
+        [Required]
+        public string CategoryName { get; set; } = null!;
+        [Required]
+        public string Description { get; set; }
+        public string? Status { get; set; }
+    }
+}
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Eproject2025.Models;
 
 public partial class Category
 {
     public string CategoryId { get; set; } = null!;
-
+    [Required]
     public string CategoryName { get; set; } = null!;
-
+    [Required]
     public string? Description { get; set; }
 
     public string? Status { get; set; }
